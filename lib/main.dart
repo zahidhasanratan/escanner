fe017814f4e915c41b4502eb2807f58dc21a0edd
zahid <zahid.hasan6@gmail.com>
2023-08-07 23:50:41 +0600

    Login Validation Completed with PHP

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:http/http.dart' as http;
 
-
 void main() {
   runApp(MyApp());
 }
@@ -16,10 +15,117 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'ESCANNER',
       home: AnimatedSplashScreen(
-        splash: Image.asset('assets/scanner.gif'), // Replace 'your_logo.gif' with your animated logo file
-        nextScreen: HomeScreen(), // Replace HomeScreen() with the main screen of your app
+        splash: Image.asset('assets/scanner.gif'),
+        nextScreen: LoginScreen(),
         splashTransition: SplashTransition.fadeTransition,
-        duration: 3000, // Duration in milliseconds for how long the splash screen should be shown
+        duration: 3000,
+      ),
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController _userIdController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  bool _isLoading = false;
+  String _loginMessage = '';
+
+  Future<void> _performLogin() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    final url = 'https://backup.zahid.com.bd/Junior/booth1/login.php';
+    final response = await http.post(Uri.parse(url), body: {
+      'user_id': _userIdController.text,
+      'password': _passwordController.text,
+    });
+
+    setState(() {
+      _isLoading = false;
+    });
+
+    if (response.statusCode == 200) {
+      final jsonData = jsonDecode(response.body);
+      final bool success = jsonData['success'];
+      final String message = jsonData['message'];
+
+      if (success) {
+        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomeScreen()));
+      } else {
+        setState(() {
+          _loginMessage = message;
+        });
+      }
+    } else {
+      setState(() {
+        _loginMessage = 'An error occurred. Please try again later.';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+        backgroundColor: Colors.black,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _userIdController,
+              decoration: InputDecoration(
+                labelText: 'User ID',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _isLoading ? null : _performLogin,
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green,
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                elevation: 8,
+              ),
+              child: _isLoading
+                  ? CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              )
+                  : Text(
+                'Login',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              _loginMessage,
+              style: TextStyle(color: Colors.red),
+            ),
+          ],
+        ),
       ),
     );
   }
